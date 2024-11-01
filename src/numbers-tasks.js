d3fefaab7bfe2f7178e8d2d709ae16a3628ddfec
belovadarya <belova.darya.iurevna@gmail.com>
2024-11-01 10:52:45 +0300

    feat: add getLinearEquationRoot solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -89,8 +89,14 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  let result;
+  if (a >= 1) {
+    result = -b / a;
+  } else if (a === 0 && b === 0) {
+    result = 0;
+  }
+  return result;
 }
 
 /**
